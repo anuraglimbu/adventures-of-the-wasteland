6141c4972a7d9f8aefe6afe857330a1dd94eedd3
vishaljain08 <47854428+vishaljain08@users.noreply.github.com>
2020-07-16 13:31:59 +0100

    Update Player.cpp

---
+++ Adventures of the Wasteland/Player.cpp
@@ -0,0 +1,27 @@
+#include "Player.h"
+
+Player::Player
+{
+
+}
+
+void SetPlayerName()
+{
+    PlayerName = 
+}
+void SetPlayerName(newName)
+{
+    PlayerName = newName
+    return
+}
+
+void SetPlayerHealth(newHealth)
+{
+    PlayerHealth = newHealth
+}
+void SetPlayerShield(newShield);
+void SetPlayerProgress(newShield);
+void GetPlayerName();
+void GetPlayerHealth();
+void GetPlayerShield();
+void GetPlayerProgress();
